591177fddc48e2db2ba28edcf0537c36aa241456
Abhishek Goswami ( Hiro ) <abhishek@ddproperty.com>
2018-02-26 14:54:07 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(0);
-$con=mysql_connect('localhost','root','sushmagiri') or die(mysql_error());
+$con=mysql_connect('localhost','root','') or die(mysql_error());
 $db='nitdemo2';
 mysql_select_db($db,$con);
 ?>
